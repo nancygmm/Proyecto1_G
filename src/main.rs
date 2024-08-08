3e4a7123b1403f36281cf987af7cf8787b2bad90
Nancy Mazariegos <nancymazariegos03@gmail.com>
2024-08-07 22:14:21 -0600

    Maze 2D

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,84 @@
-fn main() {
-    println!("Hello, world!");
+
+mod framebuffer;
+mod maze;
+mod player;
+mod caster;
+
+use minifb::{ Window, WindowOptions, Key };
+use nalgebra_glm::{Vec2};
+use std::f32::consts::PI;
+use std::time::Duration;
+use crate::framebuffer::Framebuffer;
+use crate::maze::load_maze;
+use crate::player::{Player};
+use crate::caster::{cast_ray};
+
+fn draw_cell(framebuffer: &mut Framebuffer, xo: usize, yo: usize, block_size: usize, cell: char) {
+    if cell == ' ' {
+        return;
+    }
+
+    framebuffer.set_current_color(0xFFDDDD);
+
+    for x in xo..xo + block_size {
+        for y in yo..yo + block_size {
+            framebuffer.point(x, y);
+        }
+    }
 }
+
+fn render(framebuffer: &mut Framebuffer, player: &Player) {
+    let maze = load_maze("./maze.txt");
+    let block_size = 100;
+
+    // draws maze
+    for row in 0..maze.len() {
+        for col in 0..maze[row].len() {
+            draw_cell(framebuffer, col * block_size, row * block_size, block_size, maze[row][col])
+        }
+    }
+
+    // draw player
+    framebuffer.set_current_color(0xFFDDD);
+    framebuffer.point(player.pos.x as usize, player.pos.y as usize);
+
+    // cast ray
+    cast_ray(framebuffer, &maze, &player, player.a, block_size);
+}
+
+fn main() {
+    let window_width = 1300;
+    let window_height = 900;
+    let framebuffer_width = 1300;
+    let framebuffer_height = 900;
+    let frame_delay = Duration::from_millis(16);
+
+    let mut framebuffer = Framebuffer::new(framebuffer_width, framebuffer_height);
+
+    let mut window = Window::new(
+        "Maze Runner",
+        window_width,
+        window_height,
+        WindowOptions::default(),
+    ).unwrap();
+
+    framebuffer.set_background_color(0x333355);
+
+    let player = Player {
+        pos: Vec2::new(150.0, 150.0),
+        a: PI / 3.0,
+    };
+
+    while window.is_open() && !window.is_key_down(Key::Escape) {
+
+        framebuffer.clear();
+        
+        render(&mut framebuffer, &player);
+
+        window
+            .update_with_buffer(&framebuffer.buffer, framebuffer_width, framebuffer_height)
+            .unwrap();
+
+        std::thread::sleep(frame_delay);
+    }
+}   
